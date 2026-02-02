d5adb424c3c775757a45a499782d98eb8d851f24
a-i-ueo <saori.0924@softbank.ne.jp>
2026-02-02 21:33:09 +0900

    script.jsを更新

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const question=[
+const questions=[
     {
-        question:"コンピュータによる伝票処理システムがある。このシステムは、伝票データをためる待ち行列をもち、M/M/1の待ち行列モデルが適用できるものとする。平均待ち時間がT秒以上となるのは、システムの利用率が少なくとも何％以上になったときか。ここで、伝票データをためる待ち行列の特徴は次のとおりである。",
+        questions:"コンピュータによる伝票処理システムがある。このシステムは、伝票データをためる待ち行列をもち、M/M/1の待ち行列モデルが適用できるものとする。平均待ち時間がT秒以上となるのは、システムの利用率が少なくとも何％以上になったときか。ここで、伝票データをためる待ち行列の特徴は次のとおりである。",
         choices:[
-            "ア 33",
-            "イ 50",
-            "ウ 67",
-            "エ 80",
+            "伝票データは、ポアソン分布に従って到着する。",
+            "伝票データをためる数に制限はない。",
+            "1件の伝票データの処理時間は、平均T秒に指数分布に従う。",
+            "到着間隔は一定である。",
 
         ],
         answer: 0
@@ -15,13 +15,13 @@ const question=[
 const questionElement=document.getElementById("question");
 const choiceElement=document.getElementById("choices");
 
-const currentQuestion=question[0];
+const currentQuestion=questions[0];
 
 questionElement.textContent=currentQuestion.question;
 
-currentQuestion.answer.choices.array.forEach(choices=> {
+currentQuestion.answer.choices.array.forEach(choice => {
     const button=document.createElement=("button")
     button.textContent=choice;
-    button.className="choice"
+    button.className="choice";
     choiceElement.appendChild(button);
 });
