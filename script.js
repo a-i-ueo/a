9698fd88ad270fe21a5ece2bef72b7ea6fefb55b
a-i-ueo <saori.0924@softbank.ne.jp>
2026-02-04 02:16:56 +0900

    script.jsを更新

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const questions=[
     {
-        question:"問1  コンピュータによる伝票処理システムがある。このシステムは、伝票データをためる待ち行列をもち、M/M/1の待ち行列モデルが適用できるものとする。平均待ち時間がT秒以上となるのは、システムの利用率が少なくとも何％以上になったときか。ここで、伝票データをためる待ち行列の特徴は次のとおりである。\n\r\n・伝票データは、ポアソン分布に従って到着する。\r\n・伝票データをためる数に制限はない。\r\n・1件の伝票データの処理時間は、平均T秒に指数分布に従う。",
+        question:"問1   コンピュータによる伝票処理システムがある。このシステムは、伝票データをためる待ち行列をもち、M/M/1の待ち行列モデルが適用できるものとする。平均待ち時間がT秒以上となるのは、システムの利用率が少なくとも何％以上になったときか。ここで、伝票データをためる待ち行列の特徴は次のとおりである。\n\r\n・伝票データは、ポアソン分布に従って到着する。\r\n・伝票データをためる数に制限はない。\r\n・1件の伝票データの処理時間は、平均T秒に指数分布に従う。",
         choices:[
             "ア 33",
             "イ 50",
@@ -10,15 +10,27 @@ const questions=[
         ],
         answer: 1
 
+    },
+    {
+        question:"問2   AIにおける機材学習において、2クラス分類モデルの評価方法の一つであるROC曲線で用いられる偽陽性率の説明として、最も適切なものはどれかここで、分類されるデータには正しいものと間違っているものが含まれるものとする。",
+        choices:[
+            "ア ”間違い”と予測したデータのうち、実際は”正しい”データの割合",
+            "イ 実際に”間違い”であるデータに対し、誤って”正しい”と予測したデータの割合",
+            "ウ 実際に”間違い”であるデータに対し、正しく”間違い”と予測したデータの割合",
+            "エ 全データのうち、実際に正しく予測できなかったデータの割合",
+        ],
+        answer:1
     }
 ];
+
+let currentIndex=0;
 
 const questionElement=document.getElementById("question");
 const choiceElement=document.getElementById("choices");
 const resulteElement=document.getElementById("result");
 
 function showQuestion(){
-    const q =questions[0];
+    const q =questions[currentIndex];
     questionElement.textContent=q.question;
     choiceElement.innerHTML="";
     resulteElement.textContent="";
@@ -28,11 +40,24 @@ function showQuestion(){
         button.textContent=choice;
 
         button.addEventListener("click",()=>{
+            const buttons=document.querySelectorAll("#choices button");
+            buttons.forEach(b =>b.disabled=true);
+
             if(index===q.answer){
                 resulteElement.textContent="正解!";
             }else{
                 resulteElement.textContent="不正解!";
             }
+
+            setTimeout(() =>{
+                currentIndex++;
+                if(currentIndex<questions.length){
+                    showQuestion();
+                } else{
+                    questionElement.textContent="おわり!!!!";
+                    choiceElement.innerHTML="";
+                }
+            }, 1000);
         });
         choiceElement.appendChild(button);
     });
